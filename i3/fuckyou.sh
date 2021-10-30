96199ac7bd17b0b3eb8479a39e96eb9a7ab9144f
xderty1101 <58272067+xderty1101@users.noreply.github.com>
2021-10-30 15:16:24 +1300

    picture time
    yes

--- i3/fuckyou.sh
+++ i3/fuckyou.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-notify-send -i "/home/xderty/Downloads/hole/pictures/people/Ihsan/Ishan sexy face having a moment.png" "Ihsan says" "hi\n\ndid you know that linux is better than windows" ; sleep 5 ; notify-send -i "/home/xderty/Downloads/hole/pictures/people/Ihsan/Ishan sexy face having a moment.png" "Ihsan says" "also you suck" ; sleep 1 ; betterlockscreen -l
+crimeTime=$(date +%F_%H-%M-%S)
+
+notify-send -i "/home/xderty/Downloads/hole/pictures/people/Ihsan/Ishan sexy face having a moment.png" "Ihsan says" "hi\n\ndid you know that linux is better than windows"
+fswebcam -r 1280x720 -d /dev/video0 ~/whoTouchedMyComputer/$crimeTime.png
+sleep 5
+notify-send -i "/home/xderty/Downloads/hole/pictures/people/Ihsan/Ishan sexy face having a moment.png" "Ihsan says" "also you suck"
+sleep 1
+betterlockscreen -l

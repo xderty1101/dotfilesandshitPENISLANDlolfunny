557dea85c64f1aaf61a1d96600287f02c88a4be9
xderty1101 <58272067+xderty1101@users.noreply.github.com>
2021-10-30 14:09:52 +1300

    i3
    I'm going to switch to bspwm

---
+++ i3/fuckyou.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+notify-send -i "/home/xderty/Downloads/hole/pictures/people/Ihsan/Ishan sexy face having a moment.png" "Ihsan says" "hi\n\ndid you know that linux is better than windows" ; sleep 5 ; notify-send -i "/home/xderty/Downloads/hole/pictures/people/Ihsan/Ishan sexy face having a moment.png" "Ihsan says" "also you suck" ; sleep 1 ; betterlockscreen -l
